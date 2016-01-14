6de944645a0176adad6935f19026f25abf11980c
David Xu <zengwei.xu@dianrong.com>
2016-01-14 20:15:07 +0800

    add cas password encoder

--- client/src/main/java/com/dianrong/common/uniauth/client/SSPasswordEncoder.java
+++ client/src/main/java/com/dianrong/common/uniauth/client/SSPasswordEncoder.java
@@ -2,24 +2,19 @@ package com.dianrong.common.uniauth.client;
 
 import org.springframework.security.authentication.encoding.PasswordEncoder;
 
-import com.dianrong.common.uniauth.common.util.AuthUtils;
-import com.dianrong.common.uniauth.common.util.Base64;
+import com.dianrong.common.uniauth.common.util.UniPasswordEncoder;
 
 @SuppressWarnings("deprecation")
 public class SSPasswordEncoder implements PasswordEncoder {
 
 	@Override
 	public String encodePassword(String rawPass, Object salt) {
-		String passwordSalt = (salt == null ? null : salt.toString());
-		byte[] salts = Base64.decode(passwordSalt);
-		
-		byte[] saltedPass = AuthUtils.digest(rawPass, salts);
-		return Base64.encode(saltedPass);
+		return UniPasswordEncoder.encodePassword(rawPass, salt);
 	}
 
 	@Override
 	//encPass from db
 	public boolean isPasswordValid(String encPass, String rawPass, Object salt) {
-		return  encPass.equals(encodePassword(rawPass, salt));
+		return UniPasswordEncoder.isPasswordValid(encPass, rawPass, salt);
 	}
 }
