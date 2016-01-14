07a341a92c6a36c1137e1e698b00324b27f04a99
David Xu <zengwei.xu@dianrong.com>
2016-01-14 15:08:55 +0800

    add cashelper project

---
+++ cashelper/src/main/java/com/dianrong/common/uniauth/cas/MessageService.java
@@ -0,0 +1,11 @@
+package com.dianrong.common.uniauth.cas;
+
+import org.jasig.cas.authentication.handler.support.AbstractUsernamePasswordAuthenticationHandler;
+
+public class MessageService {
+ 
+    public String getMessage() {
+    	AbstractUsernamePasswordAuthenticationHandler s;
+        return "Hello World!";
+    }
+}
