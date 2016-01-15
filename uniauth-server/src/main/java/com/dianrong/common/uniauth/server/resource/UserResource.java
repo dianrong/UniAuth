cc8eb5815309d78fdaea609615bf9734e5b96aed
chinese name : 钱晟龙 english name: Arc Qian <286999915@qq.com>
2016-01-15 20:27:05 +0800

    AOP enable.

--- uniauth-server/src/main/java/com/dianrong/common/uniauth/server/resource/UserResource.java
+++ uniauth-server/src/main/java/com/dianrong/common/uniauth/server/resource/UserResource.java
@@ -4,14 +4,16 @@ import com.dianrong.common.uniauth.common.bean.Response;
 import com.dianrong.common.uniauth.common.bean.request.UserQuery;
 import com.dianrong.common.uniauth.common.interfaces.IUserResource;
 import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * Created by Arc on 14/1/16.
  */
-@Component
+@RestController
 public class UserResource implements IUserResource {
     @Override
     public Response<String> testResult(UserQuery userQuery) {
-        return Response.success();
+        throw new RuntimeException();
+        //return Response.success();
     }
 }
