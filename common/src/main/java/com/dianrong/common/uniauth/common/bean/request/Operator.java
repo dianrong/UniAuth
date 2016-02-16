9630bbaa3937bb37b7bf2e8d1275a42f0d828dc4
David Xu <zengwei.xu@dianrong.com>
2016-02-16 16:59:24 +0800

    refactor Operator

--- common/src/main/java/com/dianrong/common/uniauth/common/bean/request/Operator.java
+++ common/src/main/java/com/dianrong/common/uniauth/common/bean/request/Operator.java
@@ -1,5 +1,7 @@
 package com.dianrong.common.uniauth.common.bean.request;
 
+import java.lang.reflect.Method;
+
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 
@@ -8,8 +10,27 @@ import com.fasterxml.jackson.annotation.JsonProperty;
  */
 public class Operator {
     @JsonProperty(access = JsonProperty.Access.READ_WRITE)
-    protected String opEmail;
-    @JsonProperty(access = JsonProperty.Access.READ_WRITE)
-    protected String opPhone;
+    protected String opAccount;
     
+    public Operator(){
+    	try {
+    		//SecurityContextHolder.getContext().getAuthentication().getPrincipal()
+			Class<?> schClass = Class.forName("org.springframework.security.core.context.SecurityContextHolder");
+			Method getContextMethod = schClass.getMethod("getContext", (Class<?>[])null);
+			Object securityContext = getContextMethod.invoke(null, (Object[])null);
+			if(securityContext != null){
+				Method getAuthenticationMethod = securityContext.getClass().getMethod("getAuthentication", (Class<?>[])null);
+				Object authentication = getAuthenticationMethod.invoke(null, (Object[])null);
+				if(authentication != null){
+					Method getPrincipalMethod = authentication.getClass().getMethod("getPrincipal", (Class<?>[])null);
+					Object principal = getPrincipalMethod.invoke(null, (Object[])null);
+					if(principal != null){
+						opAccount = principal.toString();
+					}
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+    }
 }
