aec2ea203dc776d10163327eeaecd1416ea9c593
David Xu <zengwei.xu@dianrong.com>
2016-02-27 15:33:21 +0800

    add forget password service and controller

--- cas/src/main/java/com/dianrong/common/uniauth/cas/controller/ForgetPasswordController.java
+++ cas/src/main/java/com/dianrong/common/uniauth/cas/controller/ForgetPasswordController.java
@@ -2,6 +2,7 @@ package com.dianrong.common.uniauth.cas.controller;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.AbstractController;
@@ -16,17 +17,26 @@ public class ForgetPasswordController extends AbstractController {
 	@Override
 	protected ModelAndView handleRequestInternal(HttpServletRequest request, HttpServletResponse response) throws Exception {
 		//judge by step parameter
-		//it's this controller which guarantees the security of reseting password, so need store the status of each step, maybe in session
+		//it's this controller which guarantees the security of reseting password, so need store the status of each step, in session
+		//in other words, you yourself maintain the status, and need check previous step when performing next step
+		//cannot invalidate session at the end of this process, because this will affect cas server, they are sharing a same jsessionid.
+		HttpSession session = request.getSession(false);
+		
+		System.out.println("----------------------------session:" + session);
 		String step = request.getParameter("step");
 
-		ModelAndView modelAndView = new ModelAndView("forgetPasswordStep1View");
+		ModelAndView modelAndView = new ModelAndView("inputEmailView");
 		modelAndView.addObject("name", "xuzengwei");
 		
 		ForgetPasswordModel fpm = new ForgetPasswordModel();
 		fpm.setEmail("zengwei.xu@dianrong.com");
 		
 		modelAndView.addObject("fpm", fpm);
-		forgetPasswordService.testService();
+		
+		if(session != null){
+			request.getSession().invalidate();
+		}
+		
 		return modelAndView;
 	}
 
--- cas/src/main/java/com/dianrong/common/uniauth/cas/service/ForgetPasswordService.java
+++ cas/src/main/java/com/dianrong/common/uniauth/cas/service/ForgetPasswordService.java
@@ -1,8 +1,14 @@
 package com.dianrong.common.uniauth.cas.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.dianrong.common.uniauth.cas.exp.ResetPasswordException;
+import com.dianrong.common.uniauth.common.bean.Info;
+import com.dianrong.common.uniauth.common.bean.Response;
+import com.dianrong.common.uniauth.common.bean.dto.UserDto;
 import com.dianrong.common.uniauth.common.bean.request.UserParam;
 import com.dianrong.common.uniauth.common.client.UniClientFacade;
 import com.dianrong.common.uniauth.sharerw.facade.UARWFacade;
@@ -16,13 +22,31 @@ public class ForgetPasswordService {
 	@Autowired
 	private UniClientFacade uniClientFacade;
 	
-	public void testService(){
-		for(int i = 0;i < 10000;i++){
-			System.out.println("-----------------------------------");
-		}
-		System.out.println(uarwFacade.getRoleRWResource().getAllRoleCodes());
+	public void checkUser(String email) throws ResetPasswordException {
 		UserParam userParam = new UserParam();
-		userParam.setEmail("zengwei.xu@dianrong.com");
-		System.out.println(uniClientFacade.getUserResource().getSingleUser(userParam));
+		userParam.setEmail(email);
+		Response<UserDto> response = uniClientFacade.getUserResource().getSingleUser(userParam);
+		List<Info> infoList = response.getInfo();
+
+		checkInfoList(infoList);
+	}
+	
+	public void resetPassword(String email, String password) throws ResetPasswordException {
+		UserParam userParam = new UserParam();
+		userParam.setEmail(email);
+		userParam.setPassword(password);
+		Response<Void> response = uarwFacade.getUserRWResource().resetPassword(userParam);
+		List<Info> infoList = response.getInfo();
+
+		checkInfoList(infoList);
+	}
+	
+	private void checkInfoList(List<Info> infoList) throws ResetPasswordException {
+		if(infoList != null && !infoList.isEmpty()){
+			for(Info info: infoList){
+				String errorMsg = info.getMsg();
+				throw new ResetPasswordException(errorMsg);
+			}
+		}
 	}
 }
