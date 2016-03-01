99fd76160f2d0cd3a9e635e01c1a9211d4f641f8
chinese name : 钱晟龙 english name: Arc Qian <286999915@qq.com>
2016-03-01 10:45:19 +0800

    1. calculate the baseApi. 2. config the logout api.

--- techops/src/main/webapp/scripts/controllers/main-controller.js
+++ techops/src/main/webapp/scripts/controllers/main-controller.js
@@ -6,20 +6,7 @@ define(['../utils/constant'], function (constant) {
   var MainController = function ($scope, $log, UserService, LogoutService, $cookies, $location, $rootScope, $window, localStorageService) {
     $rootScope.pageLoaded = true;
     $rootScope.logout = function () {
-      LogoutService.logout(function (response) {
-        $cookies[window.location.hostname + '/Techops'] = '';
-        console.log(response);
-        if (response != undefined) {
-          window.location.href = response.result;
-        } else {
-          $location.url('/login');
-        }
-        //$location.url(response.redirectURL);
-      }, function (error) {
-        $cookies[window.location.hostname + '/Techops'] = '';
-        console.log(error);
-        $location.url('/login');
-      })
+      window.location = constant.logout;
     }
     $rootScope.setting = function () {
       console.log($location.absUrl());
