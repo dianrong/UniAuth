d57fbce413ed6d3dc233a7404b1fc9a7984b12c5
chinese name : 钱晟龙 english name: Arc Qian <286999915@qq.com>
2016-03-02 16:05:38 +0800

    group add template

--- techops/src/main/webapp/scripts/controllers/group/group.js
+++ techops/src/main/webapp/scripts/controllers/group/group.js
@@ -4,7 +4,35 @@ define(['../../utils/constant', '../../utils/utils'], function (constant, utils)
      * @exports controllers/User
      */
     var Controller = function ($rootScope, $scope, $location, GroupService, dialogs) {
-        $rootScope.groupSelected.name = "被选中的测试组.";
+        $scope.getTree = function(userStatus) {
+            var params = {};
+            if(!userStatus) {
+                params.onlyShowGroup = true;
+            } else {
+                params.onlyShowGroup = false;
+                params.status = userStatus;
+            }
+            GroupService.getTree(params, function (res) {
+                var result = res.data;
+                if(res.info) {
+                    $scope.treeLoading = constant.loadError;
+                    return;
+                }
+                if(!result) {
+                    $scope.treeLoading = constant.loadEmpty;
+                    return;
+                }
+
+                $scope.treeLoading = '';
+                $scope.tree = result.data;
+
+            }, function () {
+                $scope.tree = {};
+                $scope.treeLoading = constant.loadError;
+            });
+        }
+        $scope.test="group test";
+        //$rootScope.groupSelected.name = "被选中的测试组.";
     };
 
     return {
