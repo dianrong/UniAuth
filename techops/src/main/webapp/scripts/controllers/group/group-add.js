27c48a1394ac31a12cfa4fdfaab5de27c0428123
chinese name : 钱晟龙 english name: Arc Qian <286999915@qq.com>
2016-03-02 17:55:57 +0800

    add "group add controller", wait for construct the tree.

---
+++ techops/src/main/webapp/scripts/controllers/group/group-add.js
@@ -0,0 +1,36 @@
+define(['../../utils/constant'], function (constant) {
+    /**
+     * A module representing a User controller.
+     * @exports controllers/User
+     */
+    var Controller = function ($scope, GroupService) {
+        $scope.getTree = function() {
+            var params = {};
+            params.onlyShowGroup = true;
+            GroupService.getTree(params, function (res) {
+                var result = res.data;
+                if(res.info) {
+                    $scope.treeLoading = constant.loadError;
+                    return;
+                }
+                if(!result) {
+                    $scope.treeLoading = constant.loadEmpty;
+                    return;
+                }
+
+                $scope.treeLoading = '';
+                GroupService.tree.data = result;
+            }, function () {
+                GroupService.tree.data = {};
+                $scope.treeLoading = constant.loadError;
+            });
+        }
+        $scope.getTree();
+    };
+
+    return {
+        name: "GroupAddController",
+        fn: ["$scope", "GroupService", Controller]
+    };
+
+});
--- techops/src/main/webapp/scripts/controllers/group/group.js
+++ techops/src/main/webapp/scripts/controllers/group/group.js
@@ -3,41 +3,13 @@ define(['../../utils/constant', '../../utils/utils'], function (constant, utils)
      * A module representing a User controller.
      * @exports controllers/User
      */
-    var Controller = function ($rootScope, $scope, $location, GroupService, dialogs) {
-        $scope.getTree = function(userStatus) {
-            var params = {};
-            if(!userStatus) {
-                params.onlyShowGroup = true;
-            } else {
-                params.onlyShowGroup = false;
-                params.status = userStatus;
-            }
-            GroupService.getTree(params, function (res) {
-                var result = res.data;
-                if(res.info) {
-                    $scope.treeLoading = constant.loadError;
-                    return;
-                }
-                if(!result) {
-                    $scope.treeLoading = constant.loadEmpty;
-                    return;
-                }
-
-                $scope.treeLoading = '';
-                $scope.tree = result.data;
-
-            }, function () {
-                $scope.tree = {};
-                $scope.treeLoading = constant.loadError;
-            });
-        }
-        $scope.test="group test";
-        //$rootScope.groupSelected.name = "被选中的测试组.";
+    var Controller = function ($scope, GroupService) {
+        $scope.tree = GroupService.tree;
     };
 
     return {
         name: "GroupController",
-        fn: ["$rootScope", "$scope", "$location", "GroupService", "dialogs", Controller]
+        fn: ["$scope", "GroupService", Controller]
     };
 
 });
