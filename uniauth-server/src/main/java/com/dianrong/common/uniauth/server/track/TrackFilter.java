402e60a7891d1ff3a2af8919e97530a69f821750
David Xu <zengwei.xu@dianrong.com>
2016-03-09 16:24:07 +0800

    add track filter.

---
+++ uniauth-server/src/main/java/com/dianrong/common/uniauth/server/track/TrackFilter.java
@@ -0,0 +1,32 @@
+package com.dianrong.common.uniauth.server.track;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
+public class TrackFilter implements Filter {
+
+	public TrackFilter() {
+	}
+
+	@Override
+	public void init(FilterConfig filterConfig) throws ServletException {
+
+	}
+
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+
+	}
+
+	@Override
+	public void destroy() {
+
+	}
+
+}
