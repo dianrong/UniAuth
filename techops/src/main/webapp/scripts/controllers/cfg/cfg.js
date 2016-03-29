1194cec71685a35140f13caccf3394e998de670b
chinese name : 钱晟龙 english name: Arc Qian <286999915@qq.com>
2016-03-29 17:14:47 +0800

    系统配置模块查询完成. 下载文件完成 屏蔽非FILE类型配置的 上传, 下载图标

--- techops/src/main/webapp/scripts/controllers/cfg/cfg.js
+++ techops/src/main/webapp/scripts/controllers/cfg/cfg.js
@@ -1,15 +1,31 @@
-define(['../../utils/constant'], function (constant) {
+define(['../../utils/constant', '../../utils/utils'], function (constant, utils) {
     /**
      * A module representing a User controller.
      * @exports controllers/User
      */
     var Controller = function ($scope, CfgService) {
 
+        function getCfgTypes() {
+            CfgService.getAllCfgTypes().$promise.then(function(res) {
+                var cfgTypes = res.data;
+                var cfgTypesArray = [];
+                cfgTypesArray.push({code:'请选择'})
+                for(var prop in cfgTypes) {
+                    cfgTypesArray.push({id:prop, code:cfgTypes[prop]});
+                }
+                utils.generatorDropdown($scope, 'cfgTypesDropdown', cfgTypesArray, cfgTypesArray[0]);
+            });
+        }
+        getCfgTypes();
+
 
         $scope.queryConfig = function () {
             var params = $scope.cfgQuery;
             if (!params) {
                 params = {};
+            }
+            if($scope.cfgTypesDropdown && $scope.cfgTypesDropdown.option) {
+                params.cfgTypeId = $scope.cfgTypesDropdown.option.id;
             }
             params.pageNumber = 0;
             params.pageSize = 5000;
