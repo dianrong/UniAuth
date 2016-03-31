653607cd99f720a9170a72bf636590cbfad15f11
David Xu <zengwei.xu@dianrong.com>
2016-03-31 21:22:36 +0800

    fix regular expression

--- uniauth-server/src/main/java/com/dianrong/common/uniauth/server/util/RegExpUtil.java
+++ uniauth-server/src/main/java/com/dianrong/common/uniauth/server/util/RegExpUtil.java
@@ -5,19 +5,30 @@ import java.util.regex.Pattern;
 
 public class RegExpUtil {
 	
-	private static final Pattern JASON_PARAM_WITH_PWD = Pattern.compile(".*\"(password|originPassword)\":\"(.*?)\".*");
+	//add more 'password' related fields into PWD_FIELDS if necessary
+	private static final String[] PWD_FIELDS = {"password", "originPassword"}; 
+	private static Pattern[] pwdPatterns;
 	
 	private RegExpUtil(){
 		
 	}
 	
 	public static String purgePassword(String jasonParam){
+		if(pwdPatterns == null){
+			pwdPatterns = new Pattern[PWD_FIELDS.length];
+			for(int i = 0;i < PWD_FIELDS.length;i++){
+				pwdPatterns[i] = Pattern.compile("(.*?)\"" + PWD_FIELDS[i] + "\":\"(.*?)\"(.*)");
+			}
+		}
 		if(jasonParam != null){
-			Matcher m = JASON_PARAM_WITH_PWD.matcher(jasonParam);
-			if(m.find()){
-				String password = m.group(2);
-				if(password != null && !"".equals(password.trim())){
-					jasonParam = jasonParam.replace(password, "******");
+			for(int i = 0;i < pwdPatterns.length;i++){
+				Matcher m = pwdPatterns[i].matcher(jasonParam);
+				if(m.find()){
+					String password = m.group(2);
+					
+					if(password != null && !"".equals(password.trim())){
+						jasonParam = jasonParam.replace(password, "******");
+					}
 				}
 			}
 		}
@@ -25,7 +36,7 @@ public class RegExpUtil {
 	}
 	
 	public static void main(String[] args) {
-		String s =  "{\"account\":\"shuanggui.fan@dianrong.com\",\"originPassword\":\"1234\",\"ip\":\"0:0:0:0:0:0:0:1\",\"password\":\"1234\"}";
+		String s =  "{\"account\":\"shuanggui.fan@dianrong.com\",\"originPassword\":\"asfsafsafsf\",\"ip\":\"0:0:0:0:0:0:0:1\",\"password\":\"qaz\"}";
 		System.out.println(purgePassword(s));
 	}
 }
