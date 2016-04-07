355245e12b8039c08d4f39999f7b2397aca6f5fd
chinese name : 钱晟龙 english name: Arc Qian <286999915@qq.com>
2016-04-07 16:40:51 +0800

    去掉grp_tag, 新增tag_type表

--- uniauth-server/src/script/sql/rt119.sql
+++ uniauth-server/src/script/sql/rt119.sql
@@ -1,39 +1,34 @@
 ALTER TABLE `permission` ADD COLUMN `value_ext` VARCHAR(128) NULL AFTER `value`;
 
+CREATE TABLE IF NOT EXISTS `tag_type` (
+  `id` INT(11) NOT NULL AUTO_INCREMENT,
+  `code` VARCHAR(32) NOT NULL,
+  `domain_id` INT(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE INDEX `code_UNIQUE` (`code` ASC),
+  CONSTRAINT `fk_tag_type_domain`
+  FOREIGN KEY (`domain_id`)
+  REFERENCES `domain` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+  ENGINE = InnoDB;
+
 CREATE TABLE IF NOT EXISTS `tag` (
-  `id` INT(11) NOT NULL,
+  `id` INT(11) NOT NULL AUTO_INCREMENT,
   `code` VARCHAR(64) NULL,
   `status` TINYINT(3) NOT NULL,
-  `domain_id` INT(11) NOT NULL,
+  `description` VARCHAR(512) NULL,
+  `tag_type_id` INT(11) NOT NULL,
   `create_date` DATETIME NULL,
   `last_update` DATETIME NULL,
   PRIMARY KEY (`id`),
-  INDEX `fk_tag_domain_idx` (`domain_id` ASC),
   INDEX `fk_tag_code_idx` (`code` ASC),
-  CONSTRAINT `fk_tag_domain`
-    FOREIGN KEY (`domain_id`)
-    REFERENCES `domain` (`id`)
+  CONSTRAINT `fk_tag_type`
+  FOREIGN KEY (`tag_type_id`)
+  REFERENCES `tag_type` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
-ENGINE = InnoDB;
-
-CREATE TABLE IF NOT EXISTS `grp_tag` (
-  `tag_id` INT(11) NOT NULL,
-  `grp_id` INT(11) NOT NULL,
-  INDEX `fk_tag_grp_tag1_idx` (`tag_id` ASC),
-  PRIMARY KEY (`grp_id`, `tag_id`),
-  INDEX `fk_grp_tag_grp1_idx` (`grp_id` ASC),
-  CONSTRAINT `fk_tag_grp_tag1`
-    FOREIGN KEY (`tag_id`)
-    REFERENCES `tag` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_tag_grp_grp1`
-    FOREIGN KEY (`grp_id`)
-    REFERENCES `grp` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+  ENGINE = InnoDB;
 
 CREATE TABLE IF NOT EXISTS `user_tag` (
   `tag_id` INT(11) NOT NULL,
@@ -42,15 +37,17 @@ CREATE TABLE IF NOT EXISTS `user_tag` (
   INDEX `fk_user_tag_user1_idx` (`user_id` ASC),
   INDEX `fk_tag_user_tag1_idx` (`tag_id` ASC),
   CONSTRAINT `fk_user_tag_tag1`
-    FOREIGN KEY (`tag_id`)
-    REFERENCES `tag` (`id`)
+  FOREIGN KEY (`tag_id`)
+  REFERENCES `tag` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_user_tag_user1`
-    FOREIGN KEY (`user_id`)
-    REFERENCES `user` (`id`)
+  FOREIGN KEY (`user_id`)
+  REFERENCES `user` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+  ENGINE = InnoDB;
 
 ALTER TABLE `grp_path` ADD INDEX `fk_path_grp_path2_idx` (`ancestor` ASC);
+
+-- insert into tag_type(`code`) values ('TITLE');
