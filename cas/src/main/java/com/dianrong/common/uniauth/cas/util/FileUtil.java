e14d117b1eee8ed951d3f5d8e455db0830dcf026
wanglin <wanglin@wanglin-ThinkPad-T520>
2016-07-19 11:11:11 +0800

    修改i18n的国际化支持menu为通过properties文件指定

--- cas/src/main/java/com/dianrong/common/uniauth/cas/util/FileUtil.java
+++ cas/src/main/java/com/dianrong/common/uniauth/cas/util/FileUtil.java
@@ -4,58 +4,122 @@ import java.io.BufferedInputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Properties;
 
-/**.
- * 文件处理相关util.主要用于cas的子系统
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.util.Assert;
+
+/**
+ * . 文件处理相关util.主要用于cas的子系统
+ * 
  * @author wanglin
  *
  */
-public class FileUtil {
+public final class FileUtil {
 
-	/**.
-	 * 返回webApp文件夹所在路径
-	 * @return
-	 */
-	public static String getWebAppLocation(){
-		String classesLocation = FileUtil.class.getClassLoader().getResource("").getPath();
-		File tclasses = new File(classesLocation);
-		
-		// ../../classes
-		return tclasses.getParentFile().getParent();
-	}
-	
-	/**.
-	 * 
-	 * @param relativePathForWebApp 相对于webapp的相对路径
-	 * @return 读取到的文件流数据
-	 * @throws IOException  文件读取io异常
-	 */
-	public static byte[] readFiles(String relativePathForWebApp) throws IOException{
-		if(relativePathForWebApp == null){
-			relativePathForWebApp = "";
-		}
-		if(relativePathForWebApp.startsWith(File.separator)){
-			relativePathForWebApp = relativePathForWebApp.substring(1);
-		}
-		String realFilePath = getWebAppLocation()+File.separator+relativePathForWebApp;
-		File file = new File(realFilePath);
-		if(file.exists() && file.isFile()){
-			byte[] bytes = new byte[(int)file.length()];
-			BufferedInputStream bufferedInputStream= null;
-			try{
-				bufferedInputStream = new BufferedInputStream(new FileInputStream(file));
-				int r = bufferedInputStream.read( bytes );
-				if (r != file.length())
-					throw new IOException("读取文件不正确");
-			} catch(IOException ex){
-				throw ex;
-			} finally {
-				if(bufferedInputStream != null){
-					bufferedInputStream.close();
-				}
-			}
-			return bytes;
-		}
-		return null;
-	}
+    /** Logger instance. */
+    private static final Logger logger = LoggerFactory.getLogger(FileUtil.class);
+
+    /**
+     * . 返回webApp文件夹所在路径
+     * 
+     * @return
+     */
+    public static String getWebAppLocation() {
+        String classesLocation = FileUtil.class.getClassLoader().getResource("").getPath();
+        File tclasses = new File(classesLocation);
+
+        // ../../classes
+        return tclasses.getParentFile().getParent();
+    }
+
+    /**
+     * .
+     * 
+     * @param relativePathForWebApp 相对于webapp的相对路径
+     * @return 读取到的文件流数据
+     * @throws IOException 文件读取io异常
+     */
+    public static byte[] readFiles(String relativePathForWebApp) throws IOException {
+        if (relativePathForWebApp == null) {
+            relativePathForWebApp = "";
+        }
+        if (relativePathForWebApp.startsWith(File.separator)) {
+            relativePathForWebApp = relativePathForWebApp.substring(1);
+        }
+        String realFilePath = getWebAppLocation() + File.separator + relativePathForWebApp;
+        File file = new File(realFilePath);
+        if (file.exists() && file.isFile()) {
+            byte[] bytes = new byte[(int) file.length()];
+            BufferedInputStream bufferedInputStream = null;
+            try {
+                bufferedInputStream = new BufferedInputStream(new FileInputStream(file));
+                int r = bufferedInputStream.read(bytes);
+                if (r != file.length())
+                    throw new IOException("读取文件不正确");
+            } catch (IOException ex) {
+                throw ex;
+            } finally {
+                if (bufferedInputStream != null) {
+                    bufferedInputStream.close();
+                }
+            }
+            return bytes;
+        }
+        return null;
+    }
+
+    
+    /**
+     * . it equals loadProperties(filePath , null)
+     * @param filePath filePath
+     * @return LinkedHashMap<String, String>
+     */
+    public static Map<String, String> loadProperties(String filePath) {
+        return loadProperties(filePath, null);
+    }
+    
+    /**
+     * . load map from properties file. if defaultMap is null, new a LinkedHashMap
+     * 
+     * @param filePath filePath
+     * @param defaultMap
+     * @return Map<String, String>
+     */
+    public static Map<String, String> loadProperties(String filePath, Map<String, String> defaultMap) {
+        Assert.notNull(filePath, "properties filePath");
+        // 读取属性文件a.properties
+        Properties prop = new Properties();
+        InputStream in = null;
+        try {
+            in = Thread.currentThread().getContextClassLoader().getResourceAsStream(filePath);
+            prop.load(in);
+            Map<String, String> values = defaultMap;
+            if (values == null) {
+                values = new HashMap<String, String>();
+            }
+            Iterator<String> it = prop.stringPropertyNames().iterator();
+            while (it.hasNext()) {
+                String key = it.next();
+                values.put(key, prop.getProperty(key));
+            }
+            return values;
+        } catch (IOException e) {
+            logger.warn("failed to read properties from " + filePath, e);
+            throw new RuntimeException(e);
+        } finally {
+            if (in != null) {
+                try {
+                    in.close();
+                } catch (IOException e) {
+                    logger.warn("failed to close FileInputStream  " + in, e);
+                }
+            }
+        }
+    }
 }
--- cas/src/main/java/com/dianrong/common/uniauth/cas/util/I18nLanguageConstantUtil.java
+++ cas/src/main/java/com/dianrong/common/uniauth/cas/util/I18nLanguageConstantUtil.java
@@ -4,68 +4,90 @@ import java.util.ArrayList;
 import java.util.LinkedHashMap;
 import java.util.List;
 
-/**.
- * 定义i18n所有支持的语言
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * . 定义i18n所有支持的语言
+ * 
  * @author wanglin
  */
 public final class I18nLanguageConstantUtil {
-    
-    public static final LinkedHashMap<String, String> localeMap = new LinkedHashMap<String, String>(){
-        private static final long serialVersionUID = 3608107004937239778L;
-        {
-            put("zh_CN", "中文");
-            put("en_US", "English");
+
+    /**
+     * . i18n menu配置文件路径
+     */
+    private static final String menuPath = "menus.properties";
+
+    /** Logger instance. */
+    private static final Logger logger = LoggerFactory.getLogger(I18nLanguageConstantUtil.class);
+
+    public static final LinkedHashMap<String, String> localeMap;
+
+    // init
+    static {
+        try {
+            localeMap = new LinkedHashMap<String, String>(FileUtil.loadProperties(menuPath));
+        } catch (Exception ex) {
+            logger.error("failed load i18n menues from filepath : " + menuPath +", please check whether it is exsists." + ex);
+            throw new RuntimeException(ex);
         }
-    };
-    
-    /**.
-     * 辅助方法  返回当前系统支持的国际化语言
+    }
+
+    /**
+     * . 辅助方法 返回当前系统支持的国际化语言
+     * 
      * @return
      */
-    public static List<I18nContent> getAllI18nLanguages(){
-        List<I18nContent>  data = new ArrayList<I18nContent>();
-        for(String localeStr : localeMap.keySet()) {
+    public static List<I18nContent> getAllI18nLanguages() {
+        List<I18nContent> data = new ArrayList<I18nContent>();
+        for (String localeStr : localeMap.keySet()) {
             data.add(new I18nContent(localeStr, localeMap.get(localeStr), UniBundleUtil.isSelected(localeStr)));
         }
         return data;
     }
-    
+
     public static void main(String[] args) {
         System.out.println(getAllI18nLanguages());
     }
-    
+
     public static class I18nContent {
         private String localeStr;
         private String language;
         private boolean isSelected;
-        
-        public I18nContent(String localeStr, String language, boolean isSelected){
+
+        public I18nContent(String localeStr, String language, boolean isSelected) {
             this.localeStr = localeStr;
             this.language = language;
             this.isSelected = isSelected;
         }
-        
+
         public String getLocaleStr() {
             return localeStr;
         }
+
         public void setLocaleStr(String localeStr) {
             this.localeStr = localeStr;
         }
+
         public String getLanguage() {
             return language;
         }
+
         public void setLanguage(String language) {
             this.language = language;
         }
+
         public boolean isSelected() {
             return isSelected;
         }
+
         public void setSelected(boolean isSelected) {
             this.isSelected = isSelected;
         }
-        
-        public String toString(){
-            return this.localeStr+ ":" + this.language+ ":" +this.isSelected;
+
+        public String toString() {
+            return this.localeStr + ":" + this.language + ":" + this.isSelected;
         }
     }
 }
