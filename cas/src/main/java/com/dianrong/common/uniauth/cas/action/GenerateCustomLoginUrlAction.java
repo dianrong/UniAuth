bab5a7092945e995c5b77fc0b3362acbc80f334e
wanglin <wanglin@wanglin-ThinkPad-T520>
2016-09-20 14:22:59 +0800

    fix bug

--- cas/src/main/java/com/dianrong/common/uniauth/cas/action/GenerateCustomLoginUrlAction.java
+++ cas/src/main/java/com/dianrong/common/uniauth/cas/action/GenerateCustomLoginUrlAction.java
@@ -7,34 +7,45 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.webflow.execution.RequestContext;
 
+import com.dianrong.common.uniauth.cas.model.CasGetServiceTicketModel;
+import com.dianrong.common.uniauth.cas.util.WebScopeUtil;
 import com.dianrong.common.uniauth.common.cons.AppConstants;
 
-/**.
- * 用于生成用户的登陆页面的url 存放到flowscope中
+/**
+ * . 用于生成用户的登陆页面的url 存放到flowscope中
+ * 
  * @author wanglin
  */
 public class GenerateCustomLoginUrlAction {
-    /** Logger instance. */
-    private final Logger logger = LoggerFactory.getLogger(getClass());
+	/** Logger instance. */
+	private final Logger logger = LoggerFactory.getLogger(getClass());
 
-    /**
-     * Generate the login ticket.
-     *
-     * @param context the context
-     * @return <code>"generated"</code>
-     */
-    public final String generate(final RequestContext context) {
-    	// 获取lt
-    	final HttpServletRequest request = WebUtils.getHttpServletRequest(context);
-    	String lt = (String)request.getSession().getAttribute(AppConstants.CAS_CUSTOM_LOGIN_LT_KEY);
-    	//获取客户自定义页面url
-    	String baseUrl = (String)context.getFlowScope().get(AppConstants.CAS_CUSTOM_LOGIN_PAGE_KEY);
-    	// 以上的值 必须都存在
-    	if(baseUrl.contains("?")) {
-    		context.getFlowScope().put(AppConstants.CAS_CUSTOM_LOGIN_URL_KEY, baseUrl + "&lt=" + lt );
-    	} else {
-    		context.getFlowScope().put(AppConstants.CAS_CUSTOM_LOGIN_URL_KEY, baseUrl + "?lt=" + lt);
-    	}
-        return "generated";
-    }
+	/**
+	 * Generate the login ticket.
+	 *
+	 * @param context
+	 *            the context
+	 * @return <code>"generated"</code>
+	 */
+	public final String generate(final RequestContext context) {
+		// 获取lt
+		final HttpServletRequest request = WebUtils.getHttpServletRequest(context);
+		String lt = (String) request.getSession().getAttribute(AppConstants.CAS_CUSTOM_LOGIN_LT_KEY);
+		// 获取客户自定义页面url
+		String baseUrl = (String) context.getFlowScope().get(AppConstants.CAS_CUSTOM_LOGIN_PAGE_KEY);
+		// 以上的值 必须都存在
+		if (baseUrl.contains("?")) {
+			baseUrl += "&";
+		} else {
+			baseUrl += "?";
+		}
+		// lt
+		baseUrl += "lt=" + lt;
+		// captcha
+		if (WebScopeUtil.loginNeedCaptcha(request.getSession())) {
+			baseUrl += "&captchapath=" + CasGetServiceTicketModel.DEFAULT_CATCHA_RELATIVE_PATH;
+		}
+		context.getFlowScope().put(AppConstants.CAS_CUSTOM_LOGIN_URL_KEY, baseUrl);
+		return "generated";
+	}
 }
