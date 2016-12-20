a5b585261ee3bcc3e2740ce2a4147c692f644f4d
wanglin <wanglin@wanglin-ThinkPad-T520>
2016-12-20 17:07:43 +0800

    修改

--- cas/src/main/java/com/dianrong/common/uniauth/cas/registry/support/CasMultiTimeUseOrTimeoutExpirationPolicy.java
+++ cas/src/main/java/com/dianrong/common/uniauth/cas/registry/support/CasMultiTimeUseOrTimeoutExpirationPolicy.java
@@ -14,7 +14,7 @@ import com.dianrong.common.uniauth.common.cons.AppConstants;
 
 import lombok.extern.slf4j.Slf4j;
 
-@Slf4j()
+@Slf4j
 public class CasMultiTimeUseOrTimeoutExpirationPolicy extends AbstractCasExpirationPolicy{
 
     /** Serialization support. */
