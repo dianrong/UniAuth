45c6e79f1b94c7c2d2ff7a84867711672ce1efff
chinese name : 钱晟龙 english name: Arc Qian <286999915@qq.com>
2017-03-10 11:07:43 +0800

    mapper层的所有操作日志都记录下来.

--- uniauth-server/src/main/java/com/dianrong/common/uniauth/server/aop/ServerMapperAOPHandler.java
+++ uniauth-server/src/main/java/com/dianrong/common/uniauth/server/aop/ServerMapperAOPHandler.java
@@ -29,19 +29,22 @@ public class ServerMapperAOPHandler {
     @Autowired
     private TenancyService  tenancyService;
     
-    @Pointcut(value = "execution(public * com.dianrong.common.uniauth.server.data.mapper.*.insert*(..)) ")
-    public void insertMappers() {}
-    
-    @Pointcut(value = "execution(public * com.dianrong.common.uniauth.server.data.mapper.*.update*(..)) ")
-    public void updateMappers() {}
-    
-    @Pointcut(value = "execution(public * com.dianrong.common.uniauth.server.data.mapper.*.delete*(..)) ")
-    public void deleteMappers() {}
+//    @Pointcut(value = "execution(public * com.dianrong.common.uniauth.server.data.mapper.*.insert*(..)) ")
+//    public void insertMappers() {}
+//
+//    @Pointcut(value = "execution(public * com.dianrong.common.uniauth.server.data.mapper.*.update*(..)) ")
+//    public void updateMappers() {}
+//
+//    @Pointcut(value = "execution(public * com.dianrong.common.uniauth.server.data.mapper.*.delete*(..)) ")
+//    public void deleteMappers() {}
+
+		@Pointcut(value = "execution(public * com.dianrong.common.uniauth.server.data.mapper.*.*(..)) ")
+		public void mappers() {}
 
     @Pointcut(value = "!execution(public * com.dianrong.common.uniauth.server.data.mapper.AuditMapper.*(..))")
     public void notAuditMapper(){}
     
-    @Around("notAuditMapper() && (insertMappers() || updateMappers() || deleteMappers())")
+    @Around("notAuditMapper() && mappers()")
     public Object interceptMapper(ProceedingJoinPoint joinPoint) throws Throwable {
     	GlobalVar origin = RequestManager.getGlobalVar();
     	if(origin == null) return joinPoint.proceed();
