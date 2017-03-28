a06ca4daf69ecf950e9496173a973a7eda3de016
wanglin <wanglin@wanglin-ThinkPad-T520>
2017-03-28 11:41:14 +0800

    1 更新readme 文件, 解决不能启动的问题 2 api的接口访问权限控制控制开关关闭起作用

--- common/src/main/java/com/dianrong/common/uniauth/common/client/SimpleApiCtrlAccountHolder.java
+++ common/src/main/java/com/dianrong/common/uniauth/common/client/SimpleApiCtrlAccountHolder.java
@@ -2,11 +2,14 @@ package com.dianrong.common.uniauth.common.client;
 
 import com.dianrong.common.uniauth.common.util.Assert;
 
+import lombok.extern.slf4j.Slf4j;
+
 /**
  * 存放uniauth-server Api访问的权限认证的账号信息
  * 
  * @author wanglin
  */
+@Slf4j
 public class SimpleApiCtrlAccountHolder implements ApiCtrlAccountHolder {
 
     private String account;
@@ -29,7 +32,9 @@ public class SimpleApiCtrlAccountHolder implements ApiCtrlAccountHolder {
     }
 
     public void setPassword(String password) {
-        Assert.notNull(password);
-        this.password = password;
+        if (password == null) {
+            log.warn("api access account's password is null");
+        }
+        this.password = password == null? "" : password;
     }
 }
