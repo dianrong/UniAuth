ed1c13252e15551f6f4d469cd56390581bd5ca89
denghb <haibo.deng@dianrong.com>
2017-06-23 16:40:48 +0800

    优化代码

--- examples/springboot-ssclient/src/main/java/com/dianrong/uniauth/ssclient/config/WebSecurityConfiguration.java
+++ examples/springboot-ssclient/src/main/java/com/dianrong/uniauth/ssclient/config/WebSecurityConfiguration.java
@@ -3,8 +3,6 @@ package com.dianrong.uniauth.ssclient.config;
 import com.dianrong.common.uniauth.client.config.UniauthSecurityConfig;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
