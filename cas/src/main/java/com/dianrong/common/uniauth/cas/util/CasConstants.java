a6f43330184e36c1640452d7cc31d2c280a73ec9
wanglin <wanglin@wanglin-ThinkPad-T520>
2017-08-07 17:45:33 +0800

    登陆首页增加一个service的判定因子code 1 如果传入service和code,则两个都需要匹配才能被判定为对应的service 2 如果只传入了service,或者传入的code不对. 则邮箱考虑service为没有自定义登陆页面的service 3 如果根据service只匹配到一个service. 则认为该service为请求方期望的service.

--- cas/src/main/java/com/dianrong/common/uniauth/cas/util/CasConstants.java
+++ cas/src/main/java/com/dianrong/common/uniauth/cas/util/CasConstants.java
@@ -36,71 +36,74 @@ public interface CasConstants {
 
   // session
   String CAS_CAPTCHA_SESSION_KEY = "CAS_CAPTCHA_SESSION_KEY";
-  
-//tag for UserInfo edit in request
- String CAS_USERINFO_MANAGE_EDIT_KEY = "edituserinfo";
- String CAS_USERINFO_MANAGE_REQUEST_METHOD_KEY = "form_method";
- @Deprecated
- String CAS_USERINFO_MANAGE_FLOW_REQUEST_METHOD_TYPE_KEY = "user_mangage_flow_request_type";
- String CAS_USERINFO_MANAGE_OPERATE_ERRORMSG_TAG = "user_manage_errormsg";
 
- // 更新类型
- String CAS_USERINFO_MANAGE_UPDATE_METHOD_TYPE_TAG = "updatetype";
+  // tag for UserInfo edit in request
+  String CAS_USERINFO_MANAGE_EDIT_KEY = "edituserinfo";
+  String CAS_USERINFO_MANAGE_REQUEST_METHOD_KEY = "form_method";
+  @Deprecated
+  String CAS_USERINFO_MANAGE_FLOW_REQUEST_METHOD_TYPE_KEY = "user_mangage_flow_request_type";
+  String CAS_USERINFO_MANAGE_OPERATE_ERRORMSG_TAG = "user_manage_errormsg";
 
- // 用户信息获取
- String CAS_USERINFO_MANAGE_USER_ID_TAG = "id";
- String CAS_USERINFO_MANAGE_UPDATE_EMAIL_TAG = "email";
- String CAS_USERINFO_MANAGE_UPDATE_PHONE_TAG = "phone";
- String CAS_USERINFO_MANAGE_UPDATE_NAME_TAG = "name";
- String CAS_USERINFO_MANGE_UPDATE_PSWD_TAG = "password";
- String CAS_USERINFO_MANGE_UPDATE_ORIGINPSWD_TAG = "orign_password";
- 
- String CAS_USER_IDENTITY = "user_identity";
- String CAS_USER_TENANCY_ID= "user_tenancy_id";
+  // 更新类型
+  String CAS_USERINFO_MANAGE_UPDATE_METHOD_TYPE_TAG = "updatetype";
 
- // CAS登陆Captcha的session值的key
- String CAS_USER_LOGIN_CAPTCHA_VALIDATION_SESSION_KEY = "cas_login_captcha_validation_session_key";
- 
- // constants for password forget
- // dispatcher parameter
- String PSWDFORGET_DISPATCHER_STEP_KEY = "step";
- String PSWDFORGET_DISPATCHER_CONTEXTURL_KEY = "savedLoginContext";
+  // 用户信息获取
+  String CAS_USERINFO_MANAGE_USER_ID_TAG = "id";
+  String CAS_USERINFO_MANAGE_UPDATE_EMAIL_TAG = "email";
+  String CAS_USERINFO_MANAGE_UPDATE_PHONE_TAG = "phone";
+  String CAS_USERINFO_MANAGE_UPDATE_NAME_TAG = "name";
+  String CAS_USERINFO_MANGE_UPDATE_PSWD_TAG = "password";
+  String CAS_USERINFO_MANGE_UPDATE_ORIGINPSWD_TAG = "orign_password";
 
- // cas cfg refresh
- // default 120 minutes
- long CAS_CFG_CACHE_REFRESH_PERIOD_MILLES = 120L * 60L * 1000L;
+  String CAS_USER_IDENTITY = "user_identity";
+  String CAS_USER_TENANCY_ID = "user_tenancy_id";
 
- // cascfg cache model in application's key
- String CAS_CFG_CACHE_MODEL_APPLICATION_KEY = "cascfg_cache_key";
+  // CAS登陆Captcha的session值的key
+  String CAS_USER_LOGIN_CAPTCHA_VALIDATION_SESSION_KEY = "cas_login_captcha_validation_session_key";
 
- // cas cfg cfg_key list
- String CAS_CFG_KEY_LOGO = "CAS_LOGO";
- String CAS_CFG_KEY_ICON = "CAS_ICON";
- String CAS_CFG_KEY_TITLE = "CAS_TITLE";
- String CAS_CFG_KEY_ALL_RIGHT = "CAS_ALL_RIGHT";
- String CAS_CFG_KEY_LOGIN_AD_IMG = "CAS_LOGIN_AD_IMG";
- // cfg cas original cfg key prefix
- String CAS_CFG_KEY_CROSS_FILTER_ORIGIN_PREFIX = "CAS_CROSS_FILTER_ORGIN";
+  // constants for password forget
+  // dispatcher parameter
+  String PSWDFORGET_DISPATCHER_STEP_KEY = "step";
+  String PSWDFORGET_DISPATCHER_CONTEXTURL_KEY = "savedLoginContext";
 
- // define the cfg type id
- String CAS_CFG_TYPE_FILE = "FILE";
- String CAS_CFG_TYPE_TEXT = "TEXT";
+  // cas cfg refresh
+  // default 120 minutes
+  long CAS_CFG_CACHE_REFRESH_PERIOD_MILLES = 120L * 60L * 1000L;
 
- // 首页广告对应herf的cfg key的后缀
- String CAS_CFG_LOGIN_AD_HREF_SUFFIX = "_HREF";
- // 默认定义的跳转url
- String CAS_CFG_HREF_DEFALT_VAL = "#";
+  // cascfg cache model in application's key
+  String CAS_CFG_CACHE_MODEL_APPLICATION_KEY = "cascfg_cache_key";
 
- String CAS_CFG_KEY_BACKGROUND_COLOR = "CAS_BACKGROUND_COLOR";
- 
-//login ticket prefix
-String CAS_LOGIN_TICKET_PREFIX = "LT";
+  // cas cfg cfg_key list
+  String CAS_CFG_KEY_LOGO = "CAS_LOGO";
+  String CAS_CFG_KEY_ICON = "CAS_ICON";
+  String CAS_CFG_KEY_TITLE = "CAS_TITLE";
+  String CAS_CFG_KEY_ALL_RIGHT = "CAS_ALL_RIGHT";
+  String CAS_CFG_KEY_LOGIN_AD_IMG = "CAS_LOGIN_AD_IMG";
+  // cfg cas original cfg key prefix
+  String CAS_CFG_KEY_CROSS_FILTER_ORIGIN_PREFIX = "CAS_CROSS_FILTER_ORGIN";
 
-// 用户跳转的url路径
-String CAS_CUSTOM_LOGIN_PAGE_KEY = "custom_login_page_key";
-String CAS_CUSTOM_LOGIN_LT_KEY = "custom_login_lt_key";
-String CAS_CUSTOM_LOGIN_URL_KEY = "custom_login_url_key";
+  // define the cfg type id
+  String CAS_CFG_TYPE_FILE = "FILE";
+  String CAS_CFG_TYPE_TEXT = "TEXT";
 
-// Application
-String SERVER_PROCESS_ERROR = "Server process error!";
+  // 首页广告对应herf的cfg key的后缀
+  String CAS_CFG_LOGIN_AD_HREF_SUFFIX = "_HREF";
+  // 默认定义的跳转url
+  String CAS_CFG_HREF_DEFALT_VAL = "#";
+
+  String CAS_CFG_KEY_BACKGROUND_COLOR = "CAS_BACKGROUND_COLOR";
+
+  // login ticket prefix
+  String CAS_LOGIN_TICKET_PREFIX = "LT";
+
+  // 用户跳转的url路径
+  String CAS_CUSTOM_LOGIN_PAGE_KEY = "custom_login_page_key";
+  String CAS_CUSTOM_LOGIN_LT_KEY = "custom_login_lt_key";
+  String CAS_CUSTOM_LOGIN_URL_KEY = "custom_login_url_key";
+
+  // Application
+  String SERVER_PROCESS_ERROR = "Server process error!";
+
+  // login page
+  String LOGIN_PAGE_REQUEST_CODE_KEY = "code";
 }
