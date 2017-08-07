6748f4c2619407ed57b5e3f6f91f15c78ab6695d
wanglin <wanglin@wanglin-ThinkPad-T520>
2017-08-07 12:52:44 +0800

    修改JWT的bug

--- ss-client/src/main/java/com/dianrong/common/uniauth/client/custom/filter/UniauthJWTAuthenticationFilter.java
+++ ss-client/src/main/java/com/dianrong/common/uniauth/client/custom/filter/UniauthJWTAuthenticationFilter.java
@@ -46,39 +46,43 @@ public class UniauthJWTAuthenticationFilter extends AbstractAuthenticationProces
   private final UniauthJWTSecurity uniauthJWTSecurity;
 
   /**
-   * 获取JWT工具类.
+   * 获取JWT工具类, 默认值.
    */
   private JWTQuery jwtQuery = new SimpleJWTQuery();
-  
+
   /**
    * 拦截登陆的请求.
    */
   private RequestMatcher loginRequestMatcher = new AntPathRequestMatcher("/login/cas");
-  
+
   /**
    * 覆盖父类中的AuthenticationSuccessHandler.
    */
   private AuthenticationSuccessHandler localSuccessHandler;
-  
+
   /**
    * 定义一个什么都不做的AuthenticationSuccessHandler来替换父类中的AuthenticationSuccessHandler.
    * 
    */
-  private static final class EmptyAuthenticationSuccessHandler implements AuthenticationSuccessHandler {
+  private static final class EmptyAuthenticationSuccessHandler
+      implements AuthenticationSuccessHandler {
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
         Authentication authentication) throws IOException, ServletException {
       // do nothing
     }
   }
-  
+
   /**
    * 用于缓存解析的JWT信息.
    */
-  private static final ThreadLocal<JWTUserTagInfo> TAG_INFO_CACHE = new ThreadLocal<JWTUserTagInfo>();
+  private static final ThreadLocal<JWTUserTagInfo> TAG_INFO_CACHE =
+      new ThreadLocal<JWTUserTagInfo>();
 
-  public UniauthJWTAuthenticationFilter(UniauthJWTSecurity uniauthJWTSecurity) {
-    this(new JWTAuthenticationRequestMatcher(), uniauthJWTSecurity);
+  public UniauthJWTAuthenticationFilter(UniauthJWTSecurity uniauthJWTSecurity, JWTQuery jwtQuery) {
+    this(new JWTAuthenticationRequestMatcher(jwtQuery), uniauthJWTSecurity);
+    Assert.notNull(jwtQuery);
+    this.jwtQuery = jwtQuery;
   }
 
   public UniauthJWTAuthenticationFilter(RequestMatcher requiresAuthenticationRequestMatcher,
@@ -103,33 +107,32 @@ public class UniauthJWTAuthenticationFilter extends AbstractAuthenticationProces
       UniauthUserJWTInfo info = uniauthJWTSecurity.getInfoFromJwt(jwt);
       String identity = info.getIdentity();
       Long tenancyId = info.getTenancyId();
-      
+
       // Cache
       JWTUserTagInfo tagCache = new JWTUserTagInfo();
       tagCache.setIdentity(identity);
       tagCache.setTenancyId(tenancyId);
       TAG_INFO_CACHE.set(tagCache);
-      
+
       UniauthIdentityToken authRequest = new UniauthIdentityToken(identity, tenancyId);
-      return  this.getAuthenticationManager().authenticate(authRequest);
+      return this.getAuthenticationManager().authenticate(authRequest);
     } catch (LoginJWTExpiredException | InvalidJWTExpiredException e) {
       log.error("JWT is invalid!", e);
       throw new JWTInvalidAuthenticationException(jwt + " is a invalid JWT string!", e);
     }
   }
-  
+
   /**
    * 设置成功认证身份的标识.
    */
   @Override
-  protected void successfulAuthentication(HttpServletRequest request,
-      HttpServletResponse response, FilterChain chain, Authentication authResult)
-      throws IOException, ServletException {
+  protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response,
+      FilterChain chain, Authentication authResult) throws IOException, ServletException {
     // 完成登陆成功的处理流程
     super.successfulAuthentication(request, response, chain, authResult);
     JWTUserTagInfo tagInfo = TAG_INFO_CACHE.get();
     JWTWebScopeUtil.refreshJWTUserInfoTag(tagInfo, request);
-    
+
     // 判断是否继续访问,还是跳转到首页
     if (loginRequestMatcher.matches(request)) {
       // 登陆操作,需要跳转到首页去
@@ -138,13 +141,14 @@ public class UniauthJWTAuthenticationFilter extends AbstractAuthenticationProces
       // 普通访问, 继续执行Filter链
       chain.doFilter(request, response);
     }
-}
+  }
 
   public JWTQuery getJwtQuery() {
     return jwtQuery;
   }
 
   public void setJwtQuery(JWTQuery jwtQuery) {
+    Assert.notNull(jwtQuery);
     this.jwtQuery = jwtQuery;
   }
 
@@ -152,14 +156,14 @@ public class UniauthJWTAuthenticationFilter extends AbstractAuthenticationProces
    * 重写覆盖父类的setAuthenticationSuccessHandler方法.
    */
   @Override
-  public void setAuthenticationSuccessHandler(
-          AuthenticationSuccessHandler successHandler) {
-      Assert.notNull(successHandler, "successHandler cannot be null");
-      this.localSuccessHandler = successHandler;
+  public void setAuthenticationSuccessHandler(AuthenticationSuccessHandler successHandler) {
+    Assert.notNull(successHandler, "successHandler cannot be null");
+    this.localSuccessHandler = successHandler;
   }
-  
+
   /**
    * 设置登陆的请求匹配的Matcher.
+   * 
    * @param requestMatcher
    */
   public final void setLoginReuqestRequestMatcher(RequestMatcher requestMatcher) {
