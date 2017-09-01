db5b4e4dbed9a2de770a208601791c90ae64e634
chinese name : 钱晟龙 english name: Arc Qian <shenglong.qian@dianrong.com>
2017-09-01 10:15:40 +0800

    techops不能使用lombok

--- techops/src/main/java/com/dianrong/common/techops/action/SuperAdminAction.java
+++ techops/src/main/java/com/dianrong/common/techops/action/SuperAdminAction.java
@@ -4,7 +4,8 @@ import com.dianrong.common.techops.util.PureHttpRequestUtil;
 import com.dianrong.common.uniauth.common.bean.request.SuperAdminParam;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -20,8 +21,9 @@ import java.util.UUID;
  */
 @RestController
 @RequestMapping("admin")
-@Slf4j
 public class SuperAdminAction {
+
+  private static final Logger LOGGER = LoggerFactory.getLogger(SuperAdminAction.class);
 
   @Autowired
   private ObjectMapper objectMapper;
@@ -35,14 +37,14 @@ public class SuperAdminAction {
       return result;
     }
     String uuid = UUID.randomUUID().toString();
-    log.warn("adminHttpRequest " + uuid + " : " + objectMapper.writeValueAsString(superAdminParam));
+    LOGGER.warn("adminHttpRequest " + uuid + " : " + objectMapper.writeValueAsString(superAdminParam));
     if (HttpMethod.GET.equalsIgnoreCase(superAdminParam.getHttpMethod())) {
       result = PureHttpRequestUtil.sendGet(superAdminParam.getUrl(), superAdminParam.getParams(), superAdminParam.getRequestHeaders());
     }
     if (HttpMethod.POST.equalsIgnoreCase(superAdminParam.getHttpMethod())) {
       result = PureHttpRequestUtil.sendPost(superAdminParam.getUrl(), superAdminParam.getParams(), superAdminParam.getRequestHeaders());
     }
-    log.warn("adminHttpResult " + uuid + " : " + result);
+    LOGGER.warn("adminHttpResult " + uuid + " : " + result);
     return result;
   }
 
