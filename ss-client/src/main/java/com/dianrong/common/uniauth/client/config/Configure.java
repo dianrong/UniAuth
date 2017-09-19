22803708ffc01e3bc398cb54bcba5c3423e0ada0
wangw <wanfei.wang@dianrong.com>
2017-09-19 14:48:56 +0800

    精简spring boot应用启动的日志打印

--- ss-client/src/main/java/com/dianrong/common/uniauth/client/config/Configure.java
+++ ss-client/src/main/java/com/dianrong/common/uniauth/client/config/Configure.java
@@ -1,5 +1,7 @@
 package com.dianrong.common.uniauth.client.config;
 
+import com.dianrong.common.uniauth.common.exp.UniauthInvalidParamterException;
+
 /**
  * 手动构造bean对象.
  *
@@ -10,11 +12,11 @@ public interface Configure<T> {
   /**
    * Create a new T.
    *
-   * @param args创建传入的参数.
+   * @param args 创建传入的参数.
    * @return new T
    * @throws UniauthInvalidParamterException 如果传入的参数不符合要求.
    */
-  T create(Object... args);
+  T create(Object... args) throws UniauthInvalidParamterException;
 
   /**
    * Judge whether the class type is supported.
